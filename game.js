cdb09e604abc323fdd75fd8c66f1a6488617555d
vinayakdhingra19 <amartk00@gmail.com>
2020-12-25 11:44:59 +0530

    first commit

---
+++ game.js
@@ -0,0 +1,95 @@
+
+var buttonColors=["red", "blue", "green", "yellow"];
+var gamePattern=[];
+var userClickedPattern=[];
+var level=0;
+
+
+$(document).one("keypress",function() {
+    nextSequence();
+});
+
+
+$(".btn").click(function() {
+    var userChosenColour= $(this).attr("id");
+    
+    userClickedPattern.push(userChosenColour);
+    playSound(userChosenColour);
+    
+    animatePress(userChosenColour);
+
+    checkAnswer(userClickedPattern.length-1);
+});
+
+function nextSequence(){
+
+    userClickedPattern=[];
+
+    var randomNumber=Math.floor(Math.random()*4);
+    var randomChosenColor=buttonColors[randomNumber];
+    
+    level++;
+
+    $("#level-title").text("Level "+level);
+
+    gamePattern.push(randomChosenColor);
+
+    $("#" + randomChosenColor).fadeIn(100).fadeOut(100).fadeIn(100);
+
+    playSound(randomChosenColor);
+
+}
+
+function playSound(name) {
+    var audio=new Audio("sounds/" + name + ".mp3");
+    audio.play();
+}
+
+function animatePress(currentColor) {
+
+    
+        $("#"+currentColor).addClass("pressed");
+
+        setTimeout(function(){
+            $("#"+currentColor).removeClass("pressed");
+        },100);
+
+}
+
+function checkAnswer(currentLevel) {
+    if(gamePattern[currentLevel]===userClickedPattern[currentLevel]){
+        console.log("success");
+    }
+    
+    if(userClickedPattern.length===gamePattern.length){
+        setTimeout(function(){
+            nextSequence();
+        },1000);
+       
+    }
+    if(userClickedPattern[currentLevel]!==gamePattern[currentLevel]){
+        playSound("wrong");
+
+        $("body").addClass("game-over");
+
+        setTimeout(function(){
+            $("body").removeClass("game-over");
+        },200);
+
+        $("#level-title").text("Game Over, Press Any Key to Restart");
+
+        startOver();
+
+    }
+
+}
+
+function startOver() {
+     
+    level=0;
+    gamePattern=[];
+    userClickedPattern=[];
+
+}
+
+
